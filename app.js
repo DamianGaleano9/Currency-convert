337d0bc8d620b4bfbfce08239591c80ed52b0c9e
Damian Galeano <dj.damian.b@hotmail.com>
2018-07-31 20:56:15 -0500

    Messages for different amounts

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 function coin() {
 
-
     this.name;
     this.exchangeRate;
+    this.message = [
+        "There is no cash at this time",
+        "The amount is too high",
+        "The minimun withdrawal amount is 100"
+    ]
 
     this.convert = function(rode) {
         conversionResult = rode * this.exchangeRate;
+        var messageHTML;
 
-        alert(conversionResult);
+        if(conversionResult <= 0) {
+            messageHTML = this.message[0];
+        } else if (rode >= 10000 ) {
+            messageHTML = this.message[1];
+        } else if (rode <= 100) {
+            messageHTML = this.message[2];
+        } else {    
+            messageHTML = rode + "Pesos are" + conversionResult + " " + this.name;
+        }
+
+        document.getElementById('result').innerHTML = messageHTML;
 
     };
 }
@@ -16,6 +31,9 @@ var pesos = new coin();
     pesos.name = "Pesos";
     pesos.exchangeRate = 2872;
 
-    function convertCurrency() {
-        pesos.convert(2900);
-    }
+function convertCurrency() {
+    var rodeSelected = document.getElementById('rode').value;
+    pesos.convert(rodeSelected);
+}
+
+
